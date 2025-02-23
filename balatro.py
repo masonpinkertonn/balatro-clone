b5504f27ff635f2fddafc80b511b5896c2dff04d
masonpinkertonn <mason.pinkerton28@paceacademy.org>
2025-02-23 12:08:06 -0500

    Main menu updates

--- balatro.py
+++ balatro.py
@@ -4,12 +4,34 @@
 
 from balatro_asciis import *
 
+# Other imports
+
+import sys
+from time import *
+
 # Main menu
+
+def start_game():
+    pass
 
 def main_menu():
 
     print(balatro_title_text)
     print("[P]lay        [O]ptions        [Q]uit        [C]ollection")
+    usr_choice = input("\n").upper()
+    while usr_choice not in ("P", "O", "Q", "C"):
+        print("\nPlease enter a valid choice.")
+        usr_choice = input("\n").upper()
+    if usr_choice == "P":
+        start_game()
+    elif usr_choice == "O":
+        pass
+    elif usr_choice == "Q":
+        print("\nShutting down...")
+        sleep(2)
+        sys.exit()
+    elif usr_choice == "C":
+        pass
 
 # GAME
 
